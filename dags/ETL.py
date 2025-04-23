2840e06c80d1b69dd66074a996590903ddff3208
LVCaoKy <lvcaoky313@gmail.com>
2025-04-23 10:24:40 +0700

    Update ETL.py

--- dags/ETL.py
+++ dags/ETL.py
@@ -8,15 +8,16 @@ with DAG(
     start_date=datetime(2024, 3, 1),
     catchup=False
 ) as dag:
-    
+    # Chạy bước Staging trong quá trình xử lý dữ liệu
     Staging = BashOperator(
         task_id="Staging_Data_daily",
         bash_command="dbt run --project-dir /home/cao-ky/MY_FOLDER/Project/JOB_pipeline/ETL --profiles-dir /home/cao-ky/MY_FOLDER/Project/JOB_pipeline/ETL --select staging"
     )
-        
+
+    # Chạy bước Mart để tạo các bảng DIM FACT theo mô hình Star sau khi đã hoàn tất xử lí
     Mart = BashOperator(
         task_id="Mart_Data_daily",
         bash_command="dbt run --project-dir /home/cao-ky/MY_FOLDER/Project/JOB_pipeline/ETL --profiles-dir /home/cao-ky/MY_FOLDER/Project/JOB_pipeline/ETL --select mart"
     )
-    
+    # Thực hiện các thao tác từ Staging rồi đến Mart
     Staging >> Mart
